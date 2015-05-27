19a159aa9ed0652111514e3f70e1f51e2bdf7e99
Munyiri Kamau <munyirik@microsoft.com>
2015-05-27 13:27:52 -0700

    adding logger to redirect console output to file

---
+++ nodeuwp/Logger.h
@@ -0,0 +1,48 @@
+/*
+    Copyright(c) Microsoft Open Technologies, Inc. All rights reserved.
+
+    The MIT License(MIT)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions :
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <windows.h>
+#include "ILogger.h"
+#include <windows.storage.h>
+
+using namespace Windows::Storage;
+
+namespace nodeuwp 
+{
+	// Logger class routes log messages to file
+	class Logger : public node::logger::ILogger
+	{
+	public: 
+		static const Logger &GetInstance();
+		~Logger() {}
+		virtual void Log(ILogger::LogLevel logLevel, const char* str) const override;
+
+	private:
+		Logger();
+		StorageFile^ m_file;
+		static std::unique_ptr<Logger> s_pInstance;
+	};
+}
