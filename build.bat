db3bac0e27dca567aac767d24eb6f610bd340af4
Munyiri Kamau <munyirik@microsoft.com>
2015-08-19 23:42:52 -0700

    Updating build script for debug binaries

--- build.bat
+++ build.bat
@@ -68,6 +68,7 @@ if /i "%1"=="arm"             set target_arch=arm
 if /i "%1"=="x86"             set target_arch=x86
 if /i "%1"=="x64"             set target_arch=x64
 if /i "%1"=="builduwpaddon"   set builduwpaddon=1
+if /i "%1"=="debug"           set debug=1
 
 :arg-ok
 shift
@@ -91,21 +92,38 @@ set buildall=1
 :arm
 @rem build node.dll
 call "%node_dir%\vcbuild.bat" arm chakra nosign uwp-dll openssl-no-asm
+if defined debug (
+  call "%node_dir%\vcbuild.bat" arm chakra nosign uwp-dll openssl-no-asm debug
+)
 pushd %batch_dir%
 @rem build nodeuwp.dll
 set WindowsSdkDir=%programfiles(x86)%\Windows Kits\10\
 msbuild nodeuwp.sln /p:configuration=release /p:platform=arm
+if defined debug (
+  msbuild nodeuwp.sln /p:configuration=debug /p:platform=arm
+)
 @rem build uwp.node
 if defined builduwpaddon (
   pushd %uwpaddon_dir%
   node.exe %node_dir%\deps\npm\node_modules\node-gyp\bin\node-gyp.js rebuild --nodedir=%node_dir% --node_win_onecore --target_arch=arm
+  if defined debug (
+    node.exe %node_dir%\deps\npm\node_modules\node-gyp\bin\node-gyp.js build --nodedir=%node_dir% --node_win_onecore --target_arch=arm --debug
+  )
 )
 @rem copy to release directory
 if defined copyrelease (
-  echo D | xcopy /y /f "%node_dir%\Release\node.dll" "%release_dir%\ARM"
-  echo D | xcopy /y /f "%batch_dir%\ARM\release\nodeuwp\nodeuwp.dll" "%release_dir%\ARM"
+  echo D | xcopy /y /f "%node_dir%\Release\node.*" "%release_dir%\Release\ARM"
+  echo D | xcopy /y /f "%batch_dir%\ARM\release\nodeuwp\nodeuwp.*" "%release_dir%\Release\ARM"
   if defined builduwpaddon (
-    echo D | xcopy /y /f "%uwpaddon_dir%\build\release\uwp.node" "%release_dir%\ARM"
+    echo D | xcopy /y /f "%uwpaddon_dir%\build\release\uwp.*" "%release_dir%\Release\ARM"
+  )
+  if defined debug (
+    echo D | xcopy /y /f "%node_dir%\Debug\node.*" "%release_dir%\Debug\ARM"
+    echo D | xcopy /y /f "%node_dir%\Debug\*.pdb" "%release_dir%\Debug\ARM"
+    echo D | xcopy /y /f "%batch_dir%\ARM\debug\nodeuwp\nodeuwp.*" "%release_dir%\Debug\ARM"
+    if defined builduwpaddon (
+      echo D | xcopy /y /f "%uwpaddon_dir%\build\debug\uwp.*" "%release_dir%\Debug\ARM"
+    )
   )
 )
 
@@ -121,21 +139,38 @@ if not defined buildall goto end
 :x86
 @rem build node.dll
 call "%node_dir%\vcbuild.bat" x86 chakra nosign uwp-dll
+if defined debug (
+  call "%node_dir%\vcbuild.bat" x86 chakra nosign uwp-dll debug
+)
 pushd %batch_dir%
 @rem build nodeuwp.dll
 set WindowsSdkDir=%programfiles(x86)%\Windows Kits\10\
 msbuild nodeuwp.sln /p:configuration=release /p:platform=x86
+if defined debug (
+  msbuild nodeuwp.sln /p:configuration=debug /p:platform=x86
+)
 @rem build uwp.node
 if defined builduwpaddon (
   pushd %uwpaddon_dir%
   node.exe %node_dir%\deps\npm\node_modules\node-gyp\bin\node-gyp.js rebuild --nodedir=%node_dir% --node_win_onecore --target_arch=x86
+  if defined debug (
+    node.exe %node_dir%\deps\npm\node_modules\node-gyp\bin\node-gyp.js build --nodedir=%node_dir% --node_win_onecore --target_arch=x86 --debug
+  )
 )
 @rem copy to release directory
 if defined copyrelease (
-  echo D | xcopy /y /f "%node_dir%\Release\node.dll" "%release_dir%\x86"
-  echo D | xcopy /y /f "%batch_dir%\release\nodeuwp\nodeuwp.dll" "%release_dir%\x86"
+  echo D | xcopy /y /f "%node_dir%\Release\node.*" "%release_dir%\Release\x86"
+  echo D | xcopy /y /f "%batch_dir%\release\nodeuwp\nodeuwp.*" "%release_dir%\Release\x86"
   if defined builduwpaddon (
-    echo D | xcopy /y /f "%uwpaddon_dir%\build\release\uwp.node" "%release_dir%\x86"
+    echo D | xcopy /y /f "%uwpaddon_dir%\build\release\uwp.*" "%release_dir%\Release\x86"
+  )
+  if defined debug (
+    echo D | xcopy /y /f "%node_dir%\Debug\node.*" "%release_dir%\Debug\x86"
+    echo D | xcopy /y /f "%node_dir%\Debug\*.pdb" "%release_dir%\Debug\x86"
+    echo D | xcopy /y /f "%batch_dir%\debug\nodeuwp\nodeuwp.*" "%release_dir%\Debug\x86"
+    if defined builduwpaddon (
+      echo D | xcopy /y /f "%uwpaddon_dir%\build\debug\uwp.*" "%release_dir%\Debug\x86"
+    )
   )
 )
 
@@ -151,21 +186,38 @@ if not defined buildall goto end
 :x64
 @rem build node.dll
 call "%node_dir%\vcbuild.bat" x64 chakra nosign uwp-dll
+if defined debug (
+  call "%node_dir%\vcbuild.bat" x64 chakra nosign uwp-dll debug
+)
 pushd %batch_dir%
 @rem build nodeuwp.dll
 set WindowsSdkDir=%programfiles(x86)%\Windows Kits\10\
 msbuild nodeuwp.sln /p:configuration=release /p:platform=x64
+if defined debug (
+  msbuild nodeuwp.sln /p:configuration=debug /p:platform=x64
+)
 @rem build uwp.node
 if defined builduwpaddon (
   pushd %uwpaddon_dir%
   node.exe %node_dir%\deps\npm\node_modules\node-gyp\bin\node-gyp.js rebuild --nodedir=%node_dir% --node_win_onecore --target_arch=x64
+  if defined debug (
+    node.exe %node_dir%\deps\npm\node_modules\node-gyp\bin\node-gyp.js build --nodedir=%node_dir% --node_win_onecore --target_arch=x64 --debug
+  )
 )
 @rem copy to release directory
 if defined copyrelease (
-  echo D | xcopy /y /f "%node_dir%\Release\node.dll" "%release_dir%\x64"
-  echo D | xcopy /y /f "%batch_dir%\x64\release\nodeuwp\nodeuwp.dll" "%release_dir%\x64"
+  echo D | xcopy /y /f "%node_dir%\Release\node.*" "%release_dir%\Release\x64"
+  echo D | xcopy /y /f "%batch_dir%\x64\release\nodeuwp\nodeuwp.*" "%release_dir%\Release\x64"
   if defined builduwpaddon (
-    echo D | xcopy /y /f "%uwpaddon_dir%\build\release\uwp.node" "%release_dir%\x64"
+    echo D | xcopy /y /f "%uwpaddon_dir%\build\release\uwp.*" "%release_dir%\Release\x64"
+  )
+  if defined debug (
+    echo D | xcopy /y /f "%node_dir%\Debug\node.*" "%release_dir%\Debug\x64"
+    echo D | xcopy /y /f "%node_dir%\Debug\*.pdb" "%release_dir%\Debug\x64"
+    echo D | xcopy /y /f "%batch_dir%\x64\debug\nodeuwp\nodeuwp.dll" "%release_dir%\Debug\x64"
+    if defined builduwpaddon (
+      echo D | xcopy /y /f "%uwpaddon_dir%\build\debug\uwp.*" "%release_dir%\Debug\x64"
+    )
   )
 )
 
